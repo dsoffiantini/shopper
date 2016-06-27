a6dc7f5371efc60b25979f6d923a7ee9c1c19abf
dsoffiantini <dsoffiantini@gmail.com>
2016-06-27 07:42:52 -0600

    back to camp

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -59,7 +59,7 @@ $(".products-container").slick({
 
 
   $('.products').hover(function() {
-    $('.products').css('box-shadow', '0px 0px 4px 4px rgba(0,0,0,.15)');
+    $(this).css('box-shadow', '0px 0px 4px 4px rgba(0,0,0,.15)');
     $(this).find('.product-overlay').show();
   }, function() {
     $('.products').css('box-shadow', '0px 0px 6px 0px rgba(0,0,0,.15)');
