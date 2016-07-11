973cc82f2db20172dc6ecc357714b71d73a449a2
Daniel Soffiantini <Daniel Soffiantini>
2016-07-11 11:33:54 -0600

    12345

--- public/scripts/product-jQuery.js
+++ public/scripts/product-jQuery.js
@@ -27,13 +27,13 @@ $('.load-jQuery').click(function() {
     //sliders
 
 
-      $('.slider-for').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          fade: true,
-          asNavFor: '.slider-nav'
-      });
+      // $('.slider-for').slick({
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1,
+      //     arrows: false,
+      //     fade: true,
+      //     asNavFor: '.slider-nav'
+      // });
       $('.slider-nav').slick({
           slidesToShow: 3,
           slidesToScroll: 1,
